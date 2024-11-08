96253cdce65728e19c62e6bc99935510b93ef276
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-08 17:17:07 -0500

    add favoriteSlice to useAppStore with zustand

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 import { createRecipeSlice, RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoritesSlice";
 
 
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a)=>({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
 
-    ...createRecipeSlice(...a)
+    ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
 
 
 })))
