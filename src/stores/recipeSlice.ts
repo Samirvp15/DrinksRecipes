ed32f58fc26aea28869be695a68f99914cb04195
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-08 12:40:04 -0500

    add zustand and init setup

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,17 @@
+import { StateCreator } from "zustand"
+
+
+type Category = {
+
+}
+
+
+export type RecipesSliceType = {
+    categories: Category[]
+}
+
+
+export const createRecipeSlice: StateCreator<RecipesSliceType> = () => ({
+    categories: []
+})
+
---
+++ src/stores/useAppStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+import { createRecipeSlice, RecipesSliceType } from "./recipeSlice";
+
+
+
+export const useAppStore = create<RecipesSliceType>((...a)=>({
+
+    ...createRecipeSlice(...a)
+
+
+}))
